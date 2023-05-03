afbabf390dd52a3412a446fd8ffd5f9055f0344a
Huy Hoang <huyhoang160593@yahoo.com>
2023-05-03 20:43:34 +0700

    using config to fix all the vscode plugin issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import UnoCSS from '@unocss/astro';
-import presetTypography from "@unocss/preset-typography";
-import presetUno from '@unocss/preset-uno';
-import presetWebFonts from '@unocss/preset-web-fonts';
 import { defineConfig } from 'astro/config';
-
-
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap(), UnoCSS({
     injectReset: true,
-    presets: [presetUno(), presetTypography(), presetWebFonts({
-      fonts: {
-        rowdies: 'Rowdies'
-      }
-    })]
   })],
 });
